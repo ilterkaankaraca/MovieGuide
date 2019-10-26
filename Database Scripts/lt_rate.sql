fd333d9d26937ceeba5e02d5c7f402c37b89e5ec
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2019-10-26 17:06:10 +0200

    JSON and lt_rate were created.

--- Database Scripts/lt_rate.sql
+++ Database Scripts/lt_rate.sql
@@ -1,7 +1,7 @@
-CREATE TABLE LT_CATEGORY(
+CREATE TABLE LT_Rate(
 	id int not null auto_increment,
 	code varchar(255),
 	creation_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	modification_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	primary key(id)
-) comment = 'lookup table for movie categories';
+) comment = 'lookup table for movie rate';
