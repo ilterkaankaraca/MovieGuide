430173971e7c6ea4ef80fa451b6701d15c43d79d
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-11-10 13:39:37 +0100

    Update StringLiterals.cs

--- MovieGuide/StringLiterals.cs
+++ MovieGuide/StringLiterals.cs
@@ -9,23 +9,17 @@ namespace Proje
     //Kullanılacak cümlelerin bulunduğu class
     static class StringLiterals
     {
-        public static string user_name = "Kullanıcı Adı";
-        public static string password =  "Şifre" ;
-        public static string password_again = "Şifre(Tekrar)";
-        public static string remember_me = "Beni Hatırla";
-        public static string login = "Giriş";
-        public static string check_error = "Kullanıcı Adı veya Şifre Yanlış";
         public static string error = "Hata";
         public static string scan = "Tara";
-        public static string delete_all = "Tümünü Sil";
+        public static string deleteAll = "Tümünü Sil";
         public static string sure = "Emin Misiniz?";
         public static string ok = "Tamam";
         public static string cancel = "İptal";
-        public static string path_incorrect = "Girilen Dosya Yolu Hatalı";
-        public static string connection_issue = "İnternet Bağlantısı Sorunu";
-        public static string notfounds = "Bulunamayanlar";
+        public static string pathIncorrect = "Girilen Dosya Yolu Hatalı";
+        public static string connectionIssue = "İnternet Bağlantısı Sorunu";
+        public static string notFounds = "Bulunamayanlar";
         public static string title = "Başlık";
-        public static string imdb_rating = "Puan";
+        public static string imdbRating = "Puan";
         public static string year="Yıl";
         public static string runtime = "Süre";
         public static string genre = "Tür";
@@ -37,15 +31,9 @@ namespace Proje
         public static string awards = "Ödüller";
         public static string path = "Yol";
         public static string search = "Ara";
-        public static string sing_up = "Kayıt Ol";
-        public static string admin = "Yetkili";
-        public static string user = "Kullanıcı";
-        public static string password_error = "Girilen Şifreler Uyuşmuyor";
-        public static string success = "Başarılı";
-        public static string name_taken = "Kullanıcı Adı Kullanılıyor";
         public static string error_blank = "Boş Bırakılamaz";
         public static string notfound = "Bulunamadı";
         public static string delete = "Sil";
-        public static string selecting_path = "Dosya Yolu Seçmek İçin İki Kere Tıklayınız";
+        public static string selectPath = "Dosya Yolu Seçmek İçin İki Kere Tıklayınız";
     }
 }
