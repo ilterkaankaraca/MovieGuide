b4779d25344dc1deca91991678042f9704c2c7a9
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-12-07 09:38:01 +0100

    Update StringLiterals.cs

--- MovieGuide/StringLiterals.cs
+++ MovieGuide/StringLiterals.cs
@@ -3,31 +3,31 @@
     //Kullanılacak cümlelerin bulunduğu class
     static class StringLiterals
     {
-        public static string error = "Hata";
-        public static string scan = "Tara";
-        public static string deleteAll = "Tümünü Sil";
-        public static string sure = "Emin Misiniz?";
-        public static string ok = "Tamam";
-        public static string cancel = "İptal";
-        public static string incorrectPath = "Girilen Dosya Yolu Hatalı";
-        public static string connectionIssue = "İnternet Bağlantısı Sorunu";
-        public static string notFounds = "Bulunamayanlar";
-        public static string title = "Başlık";
-        public static string imdbRating = "Puan";
-        public static string year = "Yıl";
-        public static string runtime = "Süre";
-        public static string genre = "Tür";
-        public static string actors = "Oyuncular";
-        public static string director = "Yönetmen";
-        public static string writer = "Senaryo";
-        public static string plot = "Konu";
-        public static string country = "Ülke";
-        public static string awards = "Ödüller";
-        public static string path = "Yol";
-        public static string search = "Ara";
-        public static string errorBlank = "Boş Bırakılamaz";
-        public static string notFound = "Bulunamadı";
-        public static string delete = "Sil";
-        public static string selectPath = "Dosya Yolu Seçmek İçin İki Kere Tıklayınız";
+        public const string error = "Hata";
+        public const string scan = "Tara";
+        public const string deleteAll = "Tümünü Sil";
+        public const string sure = "Emin Misiniz?";
+        public const string ok = "Tamam";
+        public const string cancel = "İptal";
+        public const string incorrectPath = "Girilen Dosya Yolu Hatalı";
+        public const string connectionIssue = "İnternet Bağlantısı Sorunu";
+        public const string notFounds = "Bulunamayanlar";
+        public const string title = "Başlık";
+        public const string imdbRating = "Puan";
+        public const string year = "Yıl";
+        public const string runtime = "Süre";
+        public const string genre = "Tür";
+        public const string actors = "Oyuncular";
+        public const string director = "Yönetmen";
+        public const string writer = "Senaryo";
+        public const string plot = "Konu";
+        public const string country = "Ülke";
+        public const string awards = "Ödüller";
+        public const string path = "Yol";
+        public const string search = "Ara";
+        public const string errorBlank = "Boş Bırakılamaz";
+        public const string notFound = "Bulunamadı";
+        public const string delete = "Sil";
+        public const string selectPath = "Dosya Yolu Seçmek İçin İki Kere Tıklayınız";
     }
 }
