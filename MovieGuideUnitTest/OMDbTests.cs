0b5fa0f6de8839de6e986ff0c6d8d69340b0d5f5
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2021-01-19 21:05:47 +0100

    Update OMDbTests.cs

--- MovieGuideUnitTest/OMDbTests.cs
+++ MovieGuideUnitTest/OMDbTests.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
-
+using MovieGuide;
 namespace MovieGuideUnitTest
 {
-    class OMDbTests
+    [TestClass]
+    public class OMDbTests
     {
-
+        [TestMethod]
+        public void GetMovieInfo_ReturnNull()
+        {
+            //Act
+            Movie result=OMDb.GetMovieInfo("wrfea");
+            Assert.IsNotNull(result);
+        }
+        public void GetMovieInfo_ReturnNotNull()
+        {
+            //Act
+            Movie result = OMDb.GetMovieInfo("Godfather");
+            Assert.IsNotNull(result);
+        }
     }
 }
